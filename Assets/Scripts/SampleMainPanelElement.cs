4090227a1113619caec56a92cce47b76cf692721
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-06-29 14:16:54 +0800

    Update: SampleMainPanelElement Inherit the UIPanelElement logic
    Signed-off-by: DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>

--- Assets/Scripts/SampleMainPanelElement.cs
+++ Assets/Scripts/SampleMainPanelElement.cs
@@ -1,9 +1,38 @@
-﻿using UnityEngine;
+﻿using FinTOKMAK.UIStackSystem.Runtime;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
-    public class SampleMainPanelElement : MonoBehaviour
+    public class SampleMainPanelElement : UIPanelElement
     {
+        #region Public Field
+
         
+
+        #endregion
+        
+        #region UIPanelElement Callback
+        
+        public override void OnPush()
+        {
+            base.OnPush();
+        }
+
+        public override void OnPop()
+        {
+            base.OnPop();
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        #endregion
     }
 }
