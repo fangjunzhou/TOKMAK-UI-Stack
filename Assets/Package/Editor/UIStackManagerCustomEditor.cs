1e1ca0b3ce6d17b59a989c463038f6df10cafa67
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-06-29 13:09:18 +0800

    Update: UIStackManagerCustomEditor Show all the panels
    Signed-off-by: DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>

--- Assets/Package/Editor/UIStackManagerCustomEditor.cs
+++ Assets/Package/Editor/UIStackManagerCustomEditor.cs
@@ -1,14 +1,41 @@
-﻿using UnityEditor;
+﻿using System;
+using FinTOKMAK.UIStackSystem.Runtime;
+using UnityEditor;
+using UnityEngine;
 
 namespace Package.Editor
 {
-    [CustomEditor(typeof(TYPE))]
-    public class UIStackManagerCustomEditor : Editor
+    [CustomEditor(typeof(UIStackManager))]
+    public class UIStackManagerCustomEditor : UnityEditor.Editor
     {
+        #region Private Field
+
+        private UIStackManager _stackManager;
+
+        private SerializedProperty _uiPanels;
+
+        #endregion
+
+        private void OnEnable()
+        {
+            _stackManager = (UIStackManager) serializedObject.targetObject;
+
+            _uiPanels = serializedObject.FindProperty("UIPanels");
+        }
+
         public override void OnInspectorGUI()
         {
-            
-            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            EditorGUILayout.LabelField("UI Panels", EditorStyles.boldLabel);
+
+            EditorGUILayout.BeginVertical("Box");
+            {
+                EditorGUILayout.PropertyField(_uiPanels);
+            }
+            EditorGUILayout.EndVertical();
+
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
