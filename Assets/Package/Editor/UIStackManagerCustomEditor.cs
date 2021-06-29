b3edd79deab08418df7ede36ba3f2170e0eaca71
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-06-29 13:07:09 +0800

    Added: UIStackManagerCustomEditor
    Signed-off-by: DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>

---
+++ Assets/Package/Editor/UIStackManagerCustomEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Package.Editor
+{
+    [CustomEditor(typeof(TYPE))]
+    public class UIStackManagerCustomEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            
+            base.OnInspectorGUI();
+        }
+    }
+}
