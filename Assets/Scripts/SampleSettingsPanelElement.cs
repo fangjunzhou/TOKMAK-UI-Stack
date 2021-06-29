5fe3b4ddb3d244e51055860ae196d1a9adaa4d5d
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-06-29 15:20:14 +0800

    Update: SampleSettingsPanelElement Added some logic in the settings panel Added response to the stack operations
    Signed-off-by: DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>

--- Assets/Scripts/SampleSettingsPanelElement.cs
+++ Assets/Scripts/SampleSettingsPanelElement.cs
@@ -1,9 +1,61 @@
-﻿using UnityEngine;
+﻿using FinTOKMAK.UIStackSystem.Runtime;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
-    public class SampleSettingsPanelElement : MonoBehaviour
+    public class SampleSettingsPanelElement : UIPanelElement
     {
+        #region Public Methods
+
+        /// <summary>
+        /// Call this method to close the settings panel.
+        /// The method will check if settings panel is on the top of the stack.
+        /// Otherwise, it will do nothing
+        /// </summary>
+        public void CloseSettingsPanelIfOnTop()
+        {
+            if (panelRootManager.Peek() == this)
+            {
+                panelRootManager.Pop();
+            }
+        }
+
+        #endregion
         
+        #region UIPanelElement Callback
+        
+        public override void OnPush()
+        {
+            base.OnPush();
+            
+            // Activate self
+            gameObject.SetActive(true);
+        }
+
+        public override void OnPop()
+        {
+            base.OnPop();
+            
+            // Deactivate self
+            gameObject.SetActive(false);
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            
+            // Deactivate self
+            gameObject.SetActive(false);
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            
+            // Activate self
+            gameObject.SetActive(true);
+        }
+
+        #endregion
     }
 }
