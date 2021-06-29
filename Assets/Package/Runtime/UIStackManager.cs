0670753ddefa0d7e0018938474d76687a52b8310
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-06-29 13:07:59 +0800

    Update: UIStackManager MonoBehaviour Added Int-UIPanelElement Serializable Dictionary to store all the panels
    Signed-off-by: DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>

--- Assets/Package/Runtime/UIStackManager.cs
+++ Assets/Package/Runtime/UIStackManager.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using NaughtyAttributes;
+using UnityEngine;
 
 namespace FinTOKMAK.UIStackSystem.Runtime
 {
+    /// <summary>
+    /// The SerializableDictionary that has int value as keys and UIPanelElements as values
+    /// </summary>
+    [System.Serializable]
+    public class IntUIPanelElementDict : SerializableDictionary<int, UIPanelElement>{}
+    
+    /// <summary>
+    /// This is the Stack Manager for the UI Stack System
+    /// This MonoBehaviour should be attached to every root GameObject of UIPanelElements
+    /// </summary>
     public class UIStackManager : MonoBehaviour
     {
+        #region Public Field
         
+        public IntUIPanelElementDict UIPanels;
+        
+        #endregion
     }
 }
