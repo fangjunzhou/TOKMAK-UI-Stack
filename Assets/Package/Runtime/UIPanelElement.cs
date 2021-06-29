f50e6069ee23300925ad06d0bc311850bffbcb27
DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>
2021-06-29 10:58:19 +0800

    Added: UIPanelElement base class
    Signed-off-by: DESKTOP-EDENOHJ\Fangjun Zhou <fzhou48@wisc.edu>

---
+++ Assets/Package/Runtime/UIPanelElement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinTOKMAK.UIStackSystem.Runtime
+{
+    /// <summary>
+    /// This is the base class for UIPanel
+    /// All the UI panel logic class should inherit this class
+    /// </summary>
+    public class UIPanelElement : MonoBehaviour
+    {
+        #region UI Stack Callback Methods
+
+        /// <summary>
+        /// This method will be called when the UIPanelElement is pushed into the UI Stack
+        /// </summary>
+        public void OnPush(){}
+        
+        /// <summary>
+        /// This method will be called when the UIPanelElement is popped out from the UI Stack
+        /// </summary>
+        public void OnPop(){}
+        
+        /// <summary>
+        /// This method will be called when the UIPanelElement is paused
+        /// To be specific, when current UIPanelElement is on the top of the UI Stack,
+        /// the UIPanelElement is not paused
+        /// When there's a new UIPanelElement pushed into the stack and current is no longer at the top of the stack,
+        /// OnPause callback method will be called
+        /// </summary>
+        public void OnPause(){}
+        
+        /// <summary>
+        /// This method will be called when the UIPanelElement is resumed
+        /// To be specific, when current UIPanelElement is not on the top of the UI Stack,
+        /// the UIPanelElement is paused
+        /// When the UIPanelElement on top of current panel is popped out,
+        /// and current UIPanelElement is on the top of the stack,
+        /// OnResume callback method will be called
+        /// </summary>
+        public void OnResume(){}
+
+        #endregion
+    }
+}
